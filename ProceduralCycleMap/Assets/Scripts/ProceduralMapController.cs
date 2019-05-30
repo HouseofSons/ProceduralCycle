fb3893531f24f5d15d02fc0143704e0ba86aa1e8
3DWorldRunner <andrew.searson@gmail.com>
2019-05-29 23:15:45 -0700

    added Procedural Map Building Script

---
+++ ProceduralCycleMap/Assets/Scripts/ProceduralMapController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralMapController : MonoBehaviour
+{
+    public int roomCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomCount = 10;
+    }
+
+}
