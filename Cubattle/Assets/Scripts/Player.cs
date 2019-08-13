a0c5664660bbcf58694885da7e1b8c7c69235714
3DWorldRunner <andrew.searson@gmail.com>
2019-08-12 22:54:34 -0700

    Revert "tweaking colliders"
    This reverts commit 3237c25e72ca730ca3da27775b72732c5aec6fc1.

--- Cubattle/Assets/Scripts/Player.cs
+++ Cubattle/Assets/Scripts/Player.cs
@@ -20,12 +20,7 @@ public class Player : MonoBehaviour {
 	private int jumpFrameCount;
 	private bool beenGrounded;
     private Vector3 prevPosition;
-    private int[] sides;
-
-    GameObject corner_1;
-    GameObject corner_2;
-    GameObject corner_3;
-    GameObject corner_4;
+    private List<int> sides;
 
     void Start ()
     {
@@ -33,15 +28,10 @@ public class Player : MonoBehaviour {
         playerTransform = this.gameObject.transform;
         prevPosition = this.gameObject.transform.position;
         UpdateSidesToLayers();
+        sides = new List<int>();
 
         playerInactive = true;
         PlayerToggleActive();
-
-        corner_1 = GameObject.Find("1");
-        corner_2 = GameObject.Find("2");
-        corner_3 = GameObject.Find("3");
-        corner_4 = GameObject.Find("4");
-
     }
 
 	void Update ()
@@ -54,29 +44,29 @@ public class Player : MonoBehaviour {
 
             CheckEdgeCollision();
 
-            //if (IsGrounded() && jumpFrameCount == 0)
-            //{
-            //    if (!beenGrounded)
-            //    {
-            //        beenGrounded = true;
-            //    }
-            //gravity = Vector3.zero;
-            //    jumpCount = 0;
-            //}
-            //else
-            //{
-            //    beenGrounded = false;
-            gravity += -playerTransform.up * fallSpeed * Time.deltaTime;
-            //}
+            if (IsGrounded() && jumpFrameCount == 0)
+            {
+                if (!beenGrounded)
+                {
+                    beenGrounded = true;
+                }
+                gravity = Vector3.zero;
+                jumpCount = 0;
+            }
+            else
+            {
+                beenGrounded = false;
+                gravity += -playerTransform.up * fallSpeed * Time.deltaTime;
+            }
 
-            //if (jumpFrameCount > 0)
-            //{
-            //    jumpFrameCount++;
-            //    if (jumpFrameCount > 2)
-            //    {
-            //        jumpFrameCount = 0;
-            //    }
-            //}
+            if (jumpFrameCount > 0)
+            {
+                jumpFrameCount++;
+                if (jumpFrameCount > 2)
+                {
+                    jumpFrameCount = 0;
+                }
+            }
 
             ////Drop Through Ledges
             //if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.Space))
@@ -111,27 +101,23 @@ public class Player : MonoBehaviour {
 	private bool IsGrounded()
     {
 
-        Ray ray = new Ray();
-        ray.origin = this.gameObject.transform.position;
-        ray.direction = -playerTransform.up;
-        RaycastHit hit;
-        if (Physics.SphereCast(ray, controller.radius, out hit, groundRayLength, 1 << 9)) //needs to adjust to bottom layer when map turned
-        {
-            if (!beenGrounded)
-            {
-                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position,
-                    this.gameObject.transform.position - playerTransform.up * 3.0f,
-                    Mathf.Abs(Vector3.Scale(this.gameObject.transform.position - playerTransform.up * controller.radius
-                        , playerTransform.up).magnitude -
-                        Vector3.Scale(hit.point, playerTransform.up).magnitude) - 0.1f);
-            }
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+		Ray ray = new Ray ();
+		ray.origin = this.gameObject.transform.position;
+		ray.direction = -playerTransform.up;
+		RaycastHit hit;
+		if (Physics.SphereCast(ray,controller.radius,out hit, groundRayLength, 1 << 9)) {
+			if(!beenGrounded) {
+				this.gameObject.transform.position = Vector3.MoveTowards (this.gameObject.transform.position,
+					this.gameObject.transform.position - playerTransform.up * 3.0f,
+					Mathf.Abs (Vector3.Scale(this.gameObject.transform.position - playerTransform.up * controller.radius
+						, playerTransform.up).magnitude -
+						Vector3.Scale(hit.point, playerTransform.up).magnitude) - 0.1f);
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
 
     private void CheckEdgeCollision()
     {
@@ -150,62 +136,54 @@ public class Player : MonoBehaviour {
         ray.direction = playerTransform.forward;
 
         //north east ray
-        ray.origin = playerTransform.position +
-            (playerTransform.up * playerTransform.lossyScale.y) + (playerTransform.right * playerTransform.lossyScale.x) -
+        ray.origin = playerTransform.position + (Vector3.Normalize(playerVelocity) + playerTransform.lossyScale) +
+            (playerTransform.up * (playerTransform.lossyScale.y / 2.0f)) + (playerTransform.right * (playerTransform.lossyScale.x / 2.0f)) -
             (playerTransform.forward * (LevelManager.GridSize + 1) * LevelManager.BlockSize);
-
-        corner_1.transform.position = ray.origin;
-        if (Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2))
+        if(!Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2, 1 << 9))
         {
             ToggleLayerCollision(playerTransform.position + ray.origin);
         }
         //north west ray
-        ray.origin = playerTransform.position +
-            (playerTransform.up * playerTransform.lossyScale.y) - (playerTransform.right * playerTransform.lossyScale.x) -
+        ray.origin = playerTransform.position + (Vector3.Normalize(playerVelocity) + playerTransform.lossyScale) +
+            (playerTransform.up * (playerTransform.lossyScale.y / 2.0f)) - (playerTransform.right * (playerTransform.lossyScale.x / 2.0f)) -
             (playerTransform.forward * (LevelManager.GridSize + 1) * LevelManager.BlockSize);
-
-        corner_2.transform.position = ray.origin;
-        if (Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2))
+        if (!Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2, 1 << 9))
         {
             ToggleLayerCollision(playerTransform.position + ray.origin);
         }
         //south east ray
-        ray.origin = playerTransform.position -
-            (playerTransform.up * playerTransform.lossyScale.y) + (playerTransform.right * playerTransform.lossyScale.x) -
+        ray.origin = playerTransform.position + (Vector3.Normalize(playerVelocity) + playerTransform.lossyScale) -
+            (playerTransform.up * (playerTransform.lossyScale.y / 2.0f)) + (playerTransform.right * (playerTransform.lossyScale.x / 2.0f)) -
             (playerTransform.forward * (LevelManager.GridSize + 1) * LevelManager.BlockSize);
-
-        corner_3.transform.position = ray.origin;
-        if (Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2))
+        if (!Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2, 1 << 9))
         {
             ToggleLayerCollision(playerTransform.position + ray.origin);
         }
         //south west ray
-        ray.origin = playerTransform.position -
-            (playerTransform.up * playerTransform.lossyScale.y) - (playerTransform.right * playerTransform.lossyScale.x) -
+        ray.origin = playerTransform.position + (Vector3.Normalize(playerVelocity) + playerTransform.lossyScale) -
+            (playerTransform.up * (playerTransform.lossyScale.y / 2.0f)) - (playerTransform.right * (playerTransform.lossyScale.x / 2.0f)) -
             (playerTransform.forward * (LevelManager.GridSize + 1) * LevelManager.BlockSize);
-
-        corner_4.transform.position = ray.origin;
-        if (Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2))
+        if (!Physics.Raycast(ray, ((LevelManager.GridSize + 1) * LevelManager.BlockSize) * 2, 1 << 9))
         {
-            ToggleLayerCollision(ray.origin - playerTransform.position);
+            ToggleLayerCollision(playerTransform.position + ray.origin);
         }
     }
     //called when cube not hit
     private void ToggleLayerCollision(Vector2 direction)
     {
-        if (direction.x > 0.4f)
+        if (direction.x > (playerTransform.lossyScale.x / 2.0f))
         {
             Physics.IgnoreLayerCollision(this.gameObject.layer, sides[3], true);
         }
-        if (direction.x < -0.4f)
+        if (direction.x < -(playerTransform.lossyScale.x / 2.0f))
         {
             Physics.IgnoreLayerCollision(this.gameObject.layer, sides[2], true);
         }
-        if (direction.y > 0.4f)
+        if (direction.y > (playerTransform.lossyScale.y / 2.0f))
         {
             Physics.IgnoreLayerCollision(this.gameObject.layer, sides[0], true);
         }
-        if (direction.y < -0.4f)
+        if (direction.y < -(playerTransform.lossyScale.y / 2.0f))
         {
             Physics.IgnoreLayerCollision(this.gameObject.layer, sides[1], true);
         }
@@ -213,7 +191,7 @@ public class Player : MonoBehaviour {
     //Should be called whenever Player changes Orientation
     private void UpdateSidesToLayers()
     {
-        //might need to check if player is askew
+        //might to check player isn't askew
         Vector3 pRight = playerTransform.right.normalized;
         Vector3 pUp = playerTransform.up.normalized;
 
@@ -221,110 +199,110 @@ public class Player : MonoBehaviour {
         {
             if(pUp == Vector3.up)
             {
-                sides = new int[] { 10, 11, 12, 13 };
+                sides = new List<int>() { 10, 11, 12, 13 };
             } else if (pUp == Vector3.down)
             {
-                sides = new int[] { 11, 10, 12, 13 };
+                sides = new List<int>() { 11, 10, 12, 13 };
             } else if (pUp == Vector3.forward)
             {
-                sides = new int[] { 14, 15, 12, 13 };
+                sides = new List<int>() { 14, 15, 12, 13 };
             } else //back
             {
-                sides = new int[] { 15, 14, 12, 13 };
+                sides = new List<int>() { 15, 14, 12, 13 };
             }
         } else if (pRight == Vector3.left)
         {
             if (pUp == Vector3.up)
             {
-                sides = new int[] { 10, 11, 13, 12 };
+                sides = new List<int>() { 10, 11, 13, 12 };
             }
             else if (pUp == Vector3.down)
             {
-                sides = new int[] { 11, 10, 13, 12 };
+                sides = new List<int>() { 11, 10, 13, 12 };
             }
             else if (pUp == Vector3.forward)
             {
-                sides = new int[] { 14, 15, 13, 12 };
+                sides = new List<int>() { 14, 15, 13, 12 };
             }
             else //back
             {
-                sides = new int[] { 15, 14, 13, 12 };
+                sides = new List<int>() { 15, 14, 13, 12 };
             }
 
         } else if (pRight == Vector3.up)
         {
             if (pUp == Vector3.right)
             {
-                sides = new int[] { 13, 12, 11, 10 };
+                sides = new List<int>() { 13, 12, 11, 10 };
             }
             else if (pUp == Vector3.left)
             {
-                sides = new int[] { 12, 13, 11, 10 };
+                sides = new List<int>() { 12, 13, 11, 10 };
             }
             else if (pUp == Vector3.forward)
             {
-                sides = new int[] { 14, 15, 11, 10 };
+                sides = new List<int>() { 14, 15, 11, 10 };
             }
             else //back
             {
-                sides = new int[] { 15, 14, 11, 10 };
+                sides = new List<int>() { 15, 14, 11, 10 };
             }
 
         } else if (pRight == Vector3.down)
         {
             if (pUp == Vector3.right)
             {
-                sides = new int[] { 13, 12, 10, 11 };
+                sides = new List<int>() { 13, 12, 10, 11 };
             }
             else if (pUp == Vector3.left)
             {
-                sides = new int[] { 12, 13, 10, 11 };
+                sides = new List<int>() { 12, 13, 10, 11 };
             }
             else if (pUp == Vector3.forward)
             {
-                sides = new int[] { 14, 15, 10, 11 };
+                sides = new List<int>() { 14, 15, 10, 11 };
             }
             else //back
             {
-                sides = new int[] { 15, 14, 10, 11 };
+                sides = new List<int>() { 15, 14, 10, 11 };
             }
 
         } else if (pRight == Vector3.forward)
         {
             if (pUp == Vector3.right)
             {
-                sides = new int[] { 13, 12, 15, 14 };
+                sides = new List<int>() { 13, 12, 15, 14 };
             }
             else if (pUp == Vector3.left)
             {
-                sides = new int[] { 12, 13, 15, 14 };
+                sides = new List<int>() { 12, 13, 15, 14 };
             }
             else if (pUp == Vector3.up)
             {
-                sides = new int[] { 10, 11, 15, 14 };
+                sides = new List<int>() { 10, 11, 15, 14 };
             }
             else //down
             {
-                sides = new int[] { 11, 10, 15, 14 };
+                sides = new List<int>() { 11, 10, 15, 14 };
             }
 
         } else //back
         {
             if (pUp == Vector3.right)
             {
-                sides = new int[] { 13, 12, 14, 15 };
+                sides = new List<int>() { 13, 12, 14, 15 };
             }
             else if (pUp == Vector3.left)
             {
-                sides = new int[] { 12, 13, 14, 15 };
+                sides = new List<int>() { 12, 13, 14, 15 };
             }
             else if (pUp == Vector3.up)
             {
-                sides = new int[] { 10, 11, 14, 15 };
+                sides = new List<int>() { 10, 11, 14, 15 };
             }
             else //down
             {
-                sides = new int[] { 11, 10, 14, 15 };
+                sides = new List<int>() { 11, 10, 14, 15 };
             }
 
         }
