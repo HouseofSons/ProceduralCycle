603779a265b69decd75b646e9101cb0866b32ace
3DWorldRunner <andrew.searson@gmail.com>
2019-08-16 23:51:03 -0700

    tightened up code

--- Cubattle/Assets/Scripts/Block.cs
+++ Cubattle/Assets/Scripts/Block.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    public static List<Block> Blocks = new List<Block>();//might be removed in future
+    public static List<Block> Blocks = new List<Block>();
 
     public Vector3Int MapGridLocation { get; set; }
     public Vector3Int PrevMapGridLocation { get; set; }
 
-    public bool moved;
-
     void Start()
     {
-        Blocks.Add(this);//might be removed in future
-        MapGrid.UpdateBlockLocation(this,this.gameObject.transform.position);
+        Blocks.Add(this);
+        MapGrid.UpdateGridLocation(this, this.gameObject.transform.position);
     }
 
     void Update()
     {
-        if (moved)
-        {
-            MapGrid.UpdateBlockLocation(this, this.gameObject.transform.position);
-        }
+
     }
 
     public static void UpdateAllBlockColliders()
@@ -35,13 +30,11 @@ public class Block : MonoBehaviour
 
     public void UpdateBlockColliders()
     {
-        bool [] sidesEnabled;
-        
-        sidesEnabled = MapGrid.GridLocationHasNeighbors(MapGridLocation);
-        
-        for (int i = 0; i < 6; i++)
+        Block[] sidesEnabled = MapGrid.GridLocationHasNeighbors(MapGridLocation);
+
+        for (int i = 0; i < sidesEnabled.Length; i++)
         {
-            if(sidesEnabled[i])
+            if (sidesEnabled[i])
             {
                 this.gameObject.transform.GetChild(0).GetChild(i).GetComponent<MeshCollider>().enabled = false;
                 //Debug.Log(this.gameObject.name + " wall: " + this.gameObject.transform.GetChild(0).GetChild(i).name + " bool: " + false);
@@ -50,6 +43,19 @@ public class Block : MonoBehaviour
                 this.gameObject.transform.GetChild(0).GetChild(i).GetComponent<MeshCollider>().enabled = true;
                 //Debug.Log(this.gameObject.name + " wall: " + this.gameObject.transform.GetChild(0).GetChild(i).name + " bool: " + true);
             }
+        }
+    }
+    //Might be used in future to update block neighbors colliders
+    //Assumption is blocks my move during gameplay and will need to update colliders
+    public static void UpdateNeighboringBlockColliders(Block b)
+    {
+        b.UpdateBlockColliders(); //update this blocks colliders
+
+        Block[] blocksToUpdate = MapGrid.GridLocationHasNeighbors(b.MapGridLocation);
+        //update neighbors blocks colliders
+        foreach (Block btu in blocksToUpdate)
+        {
+            btu.UpdateBlockColliders();
         }
     }
 }
--- Cubattle/Assets/Scripts/MapGrid.cs
+++ Cubattle/Assets/Scripts/MapGrid.cs
@@ -13,7 +13,7 @@ public class MapGrid
         BlockSize = blockSize;
     }
 
-    public static bool UpdateBlockLocation(Block b, Vector3 location)
+    public static bool UpdateGridLocation(Block b, Vector3 location)
     {
         Vector3Int nl = new Vector3Int(Mathf.RoundToInt(location.x), Mathf.RoundToInt(location.y), Mathf.RoundToInt(location.z));
 
@@ -27,26 +27,25 @@ public class MapGrid
                 b.PrevMapGridLocation = b.MapGridLocation;
                 b.MapGridLocation = newLocation;
                 Blocks[newLocation.x, newLocation.y, newLocation.z] = b;
-                UpdateMapWalls(b);
                 return true;
             }
         }
         return false;
     }
 
-    public static bool[] GridLocationHasNeighbors(Vector3Int blockLocation)
+    public static Block[] GridLocationHasNeighbors(Vector3Int mapGridLocation)
     {
         //+X,-X,+Y,-Y,+Z,-Z
-        bool[] neighbors = new bool[6];
+        Block[] neighbors = new Block[6];
 
         Vector3Int[] neighborCheck = new Vector3Int[6];
 
-        neighborCheck[0] = new Vector3Int(blockLocation.x + 1, blockLocation.y, blockLocation.z);
-        neighborCheck[1] = new Vector3Int(blockLocation.x - 1, blockLocation.y, blockLocation.z);
-        neighborCheck[2] = new Vector3Int(blockLocation.x, blockLocation.y + 1, blockLocation.z);
-        neighborCheck[3] = new Vector3Int(blockLocation.x, blockLocation.y - 1, blockLocation.z);
-        neighborCheck[4] = new Vector3Int(blockLocation.x, blockLocation.y, blockLocation.z + 1);
-        neighborCheck[5] = new Vector3Int(blockLocation.x, blockLocation.y, blockLocation.z - 1);
+        neighborCheck[0] = new Vector3Int(mapGridLocation.x + 1, mapGridLocation.y, mapGridLocation.z);
+        neighborCheck[1] = new Vector3Int(mapGridLocation.x - 1, mapGridLocation.y, mapGridLocation.z);
+        neighborCheck[2] = new Vector3Int(mapGridLocation.x, mapGridLocation.y + 1, mapGridLocation.z);
+        neighborCheck[3] = new Vector3Int(mapGridLocation.x, mapGridLocation.y - 1, mapGridLocation.z);
+        neighborCheck[4] = new Vector3Int(mapGridLocation.x, mapGridLocation.y, mapGridLocation.z + 1);
+        neighborCheck[5] = new Vector3Int(mapGridLocation.x, mapGridLocation.y, mapGridLocation.z - 1);
 
         for (int i = 0; i < neighborCheck.Length; i++)
         {
@@ -54,85 +53,9 @@ public class MapGrid
                 neighborCheck[i].y < 0 || neighborCheck[i].y > LevelManager.GridSize - 1 ||
                 neighborCheck[i].z < 0 || neighborCheck[i].z > LevelManager.GridSize - 1))
             {
-                if (Blocks[neighborCheck[i].x, neighborCheck[i].y, neighborCheck[i].z] == null)
-                {
-                    neighbors[i] = false;
-                } else
-                {
-                    neighbors[i] = true;
-                }
+                neighbors[i] = Blocks[neighborCheck[i].x, neighborCheck[i].y, neighborCheck[i].z];
             }
         }
         return neighbors;
-    }
-
-    public static void UpdateMapWalls(Block b)
-    {
-        List<Block> blocksToUpdate = GridLocationColumnNeighborBlocks(b.MapGridLocation, Mathf.Abs(LevelManager.FacingCoordinate));
-        blocksToUpdate.AddRange(GridLocationColumnNeighborBlocks(b.PrevMapGridLocation, Mathf.Abs(LevelManager.FacingCoordinate)));
-
-        foreach (Block btu in blocksToUpdate)
-        {
-            btu.UpdateBlockColliders();
-        }
-    }
-
-    public static List<Block> GridLocationColumnNeighborBlocks(Vector3Int blockLocation, int playerForwardCoordinate)
-    {
-        List<Block> columnNeighbors = new List<Block>();
-
-        if (playerForwardCoordinate == 0 || playerForwardCoordinate == 3)
-        {
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.y + 1, blockLocation.z, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.y - 1, blockLocation.z, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.y, blockLocation.z + 1, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.y, blockLocation.z - 1, playerForwardCoordinate));
-        }
-        else if (playerForwardCoordinate == 1 || playerForwardCoordinate == 4)
-        {
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x + 1, blockLocation.z, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x - 1, blockLocation.z, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x, blockLocation.z + 1, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x, blockLocation.z - 1, playerForwardCoordinate));
-        }
-        else /*(playerForwardCoordinate == 2 || playerForwardCoordinate == 5)*/
-        {
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x + 1, blockLocation.y, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x - 1, blockLocation.y, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x, blockLocation.y + 1, playerForwardCoordinate));
-            columnNeighbors.AddRange(ColumnBlocks(blockLocation.x, blockLocation.y - 1, playerForwardCoordinate));
-        }
-
-        return columnNeighbors;
-    }
-
-    private static List<Block> ColumnBlocks(int x, int y, int colCoord)
-    {
-        List<Block> colBlocks = new List<Block>();
-
-        if (x < 0 || x > LevelManager.GridSize - 1 ||
-            y < 0 || y > LevelManager.GridSize - 1)
-        {
-            return colBlocks;
-        }
-        int q, r, s, t, u, v;
-        if (colCoord == 0) { q = 0; r = Blocks.GetLength(0); s = x; t = x + 1; u = y; v = y + 1; }
-        else if (colCoord == 1) { q = x; r = x + 1; s = 0; t = Blocks.GetLength(1); u = y; v = y + 1; }
-        else  /*(colCoord == 2)*/  { q = x; r = x + 1; s = y; t = y + 1; u = 0; v = Blocks.GetLength(2); }
-
-        for (int i = q; i < r; i++)
-        {
-            for (int j = s; j < t; j++)
-            {
-                for (int k = u; k < v; k++)
-                {
-                    if (Blocks[i, j, k] != null)
-                    {
-                        colBlocks.Add(Blocks[i, j, k]);
-                    }
-                }
-            }
-        }
-        return colBlocks;
     }
 }
