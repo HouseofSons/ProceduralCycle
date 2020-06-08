f4a32d0093b5fd78e11c49679c501e1f14648eef
3dworldrunner <3dworldrunner@github.com>
2020-06-08 14:05:09 -0700

    added garbage

--- DungeonRun/Assets/_Scripts/MainCamera.cs
+++ DungeonRun/Assets/_Scripts/MainCamera.cs
@@ -14,16 +14,27 @@ public class MainCamera : MonoBehaviour
     void LateUpdate()
     {
         SmoothFollow();
+        SmoothPan();
     }
 
     private void SmoothFollow()
     {
         Vector3 currPos = this.transform.position;
-        Vector3 toPos = GameManager.GetCurrentPlayer().transform.position;
+        Vector3 playerPos = GameManager.GetCurrentPlayer().transform.position;
+        //Vector3 playerLookPos = new Vector3(
+        //                        AimArrow.Arrow.transform.up.x * (10 * Player.Speed) + transform.position.x,
+        //                        transform.position.y,
+        //                        AimArrow.Arrow.transform.up.z * (10 * Player.Speed) + transform.position.z);
+        Vector3 nextPartitionPos = Player.NextPartition.transform.position;
+        Vector3 currPartPos = GameManager.GetCurrentPlayer().CurrentRoom.GetPartition(playerPos).transform.position;
+        Vector3 toPos = new Vector3((nextPartitionPos.x + currPartPos.x)/2, currPos.y, (nextPartitionPos.z + currPartPos.z)/2);
 
         this.transform.position =
             Vector3.Lerp(currPos,new Vector3(toPos.x, currPos.y, toPos.z), Time.deltaTime * 2.0f);
+    }
 
+    private void SmoothPan()
+    {
         float toSize = (((Player.Speed - GameManager.SpeedMin) / (GameManager.SpeedMax - GameManager.SpeedMin)) *
             (GameManager.CameraSizeMax - GameManager.CameraSizeMin)) + GameManager.CameraSizeMin;
         this.GetComponent<Camera>().orthographicSize = Mathf.SmoothStep(this.GetComponent<Camera>().orthographicSize, toSize, 0.1f);
