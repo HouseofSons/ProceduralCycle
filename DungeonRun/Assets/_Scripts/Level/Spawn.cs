393b1b60c6017af46e5b4fd2c0c09cd3b15da778
3dworldrunner <3dworldrunner@github.com>
2020-06-08 14:04:34 -0700

    Revert "update"
    This reverts commit d78857b715d75b362236212b2e7b353f3811ea2b.

--- DungeonRun/Assets/_Scripts/Level/Spawn.cs
+++ DungeonRun/Assets/_Scripts/Level/Spawn.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     public GameObject Room;
-    public GameObject Partition;
 
     public Room GetRoom()
     {
         return Room.GetComponent<Room>();
-    }
-
-    public Partition GetPartition()
-    {
-        return Partition.GetComponent<Partition>();
     }
 }
